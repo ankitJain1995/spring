0467824ec5af3319e23cfcbe0dcc507b57bb3d43
Ankit Jain <ankit_jain2021@outlook.com>
2023-09-14 22:16:36 +0530

    SpringBoot School Project

---
+++ school_project/src/test/java/com/example/school_project/SchoolProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.school_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
