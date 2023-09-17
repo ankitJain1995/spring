c2e2ad70ca32eaf296ce3e3cad22602d15d8332b
Ankit Jain <ankit_jain2021@outlook.com>
2023-09-17 13:36:39 +0530

    Home Page Added

--- school_project/src/main/java/com/example/school_project/Controller/HomeController.java
+++ school_project/src/main/java/com/example/school_project/Controller/HomeController.java
@@ -1,14 +1,16 @@
 package com.example.school_project.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
 
-//@RequestMapping(value = {"","/","home"})
-//    public String displayHomePage(){
-//        return "home.html";
-//    }
+@RequestMapping(value = {"","/","home"})
+    public String displayHomePage(Model model){
+      model.addAttribute("username","chotu");
+        return "home.html";
+    }
 }
